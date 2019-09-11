15e6266a154f8bfad361b1e9811b2119aac113ec
Nicolas Susterman <nicosus95@hotmail.com>
2019-09-11 16:26:45 -0300

    +Character controller 2D, move forward constant, jump

---
+++ JuegoCelu/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float runSpeed = 40;
+
+    CharacterController2D controller;
+    bool isJumping = false;
+    bool isCrouching = false;
+
+    private void Awake()
+    {
+        controller = GetComponent<CharacterController2D>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+            isJumping = true;
+        else
+            isJumping = false;
+
+        if (Input.GetButtonDown("Crouch"))
+            isCrouching = true;
+        else
+            isCrouching = false;
+    }
+
+    private void FixedUpdate()
+    {
+        controller.Move(runSpeed * Time.fixedDeltaTime, isCrouching, isJumping);
+    }
+}
