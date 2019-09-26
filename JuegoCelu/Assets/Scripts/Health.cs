206db3ca14729426fd56f9ed4c1d4f927325812c
Nicolas Susterman <nicosus95@hotmail.com>
2019-09-26 15:31:21 -0300

    +Enemy, EnemyHealth

---
+++ JuegoCelu/Assets/Scripts/Health.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public virtual void TakeDamage(bool _IsDamage) { }
+    public virtual void TakeDamage(float _Damage) { }
+}
