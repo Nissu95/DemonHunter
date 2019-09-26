9296fd5a360963f35dd6fdf0889bee7acfa74048
Nicolas Susterman <nicosus95@hotmail.com>
2019-09-26 16:07:14 -0300

    +Attack do something

---
+++ JuegoCelu/Assets/Scripts/Damage_Script.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damage_Script : MonoBehaviour
+{
+    [SerializeField] string m_EnemyTag;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag(m_EnemyTag))
+        {
+            Health otherHealth = other.GetComponent<Health>();
+            otherHealth.TakeDamage(true);
+        }
+    }
+}
