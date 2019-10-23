204c6dc93047d2bfe53309e1c75faa21fd5f9b9d
Nicolas Susterman <nicosus95@hotmail.com>
2019-10-23 14:13:53 -0300

    +Reflected bullets collides with all enemies

--- JuegoCelu/Assets/Scripts/Damage_Script.cs
+++ JuegoCelu/Assets/Scripts/Damage_Script.cs
@@ -6,14 +6,13 @@ public class Damage_Script : MonoBehaviour
 {
     [SerializeField] float damage = 10;
     [SerializeField] string enemyTag;
-    [SerializeField] string turretEnemyTag;
     [SerializeField] string bulletTag;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag(enemyTag) || collision.CompareTag(turretEnemyTag))
+        if (collision.CompareTag(enemyTag))
             collision.GetComponent<Health>().TakeDamage(damage);
         else if (collision.CompareTag(bulletTag))
-            collision.GetComponent<Bullet_Script>().Reflect(turretEnemyTag);
+            collision.GetComponent<Bullet_Script>().Reflect(enemyTag);
     }
 }
